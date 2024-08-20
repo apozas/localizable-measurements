d3bae5e0ddf622f350a7860828a3a9ba1ba946ec
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2024-08-20 16:34:14 +0200

    Add equivalence under swap
    The code now looks for equivalence under Definition 2 in the manuscript. This reduces the number of unique 2-partite-3-ebit bases from 9 to 8, the number of unique 2-partite-9-ebit bases from 47 to 35, and the number of unique 3-partite-2-ebit bases from 14 to 9. The calculations for 3-partite-17-ebit bases are underway.

--- findUniqueBases.m
+++ findUniqueBases.m
@@ -2,20 +2,62 @@
 % 
 % Author: Alejandro Pozas-Kerstjens
 % This code identifies the unique (per as Definition 2 in the manuscript)
-% two-qubit bases out of a list of them. Actually it looks for equivalents
-% under local unitary rotations, but not under swap of the parties.
+% two-qubit bases out of a list of them.
 
-load('allThreeEbitSolutions.mat');
-allM = Expression1;
+load('allThreeEbitSolutions.mat');   % Change for the different cases
+allM = knownM;
 nbSolutions=1;
 knownM = zeros(size(allM,1),size(allM,2),1);
 knownM(:,:,1) = eye(size(allM,1));
 
+swap = [1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1];
+
+% Swap operators for three parties
+% swap213 = [1 0 0 0 0 0 0 0; 0 1 0 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 0 0 0 0 1 0 0;
+%            0 0 1 0 0 0 0 0; 0 0 0 1 0 0 0 0; 0 0 0 0 0 0 1 0; 0 0 0 0 0 0 0 1];
+% swap321 = [1 0 0 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 0 1 0 0 0 0 0; 0 0 0 0 0 0 1 0;
+%            0 1 0 0 0 0 0 0; 0 0 0 0 0 1 0 0; 0 0 0 1 0 0 0 0; 0 0 0 0 0 0 0 1];
+% swap132 = [1 0 0 0 0 0 0 0; 0 0 1 0 0 0 0 0; 0 1 0 0 0 0 0 0; 0 0 0 1 0 0 0 0;
+%            0 0 0 0 1 0 0 0; 0 0 0 0 0 0 1 0; 0 0 0 0 0 1 0 0; 0 0 0 0 0 0 0 1];
+% swap231 = [1 0 0 0 0 0 0 0; 0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 0 0 0 0 0 1 0;
+%            0 1 0 0 0 0 0 0; 0 0 0 1 0 0 0 0; 0 0 0 0 0 1 0 0; 0 0 0 0 0 0 0 1];
+% swap312 = [1 0 0 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 1 0 0 0 0 0 0; 0 0 0 0 0 1 0 0;
+%            0 0 1 0 0 0 0 0; 0 0 0 0 0 0 1 0; 0 0 0 1 0 0 0 0; 0 0 0 0 0 0 0 1];
+
 for i=1:size(allM,3)
     unique=1;
     for k=1:nbSolutions
-        [isrep fMin a b]=localunitarilyequiv2(allM(:,:,i),knownM(:,:,k));
-        if ~isrep [isrep fMin a b]=localunitarilyequiv2(allM(:,:,i),knownM(:,:,k));end
+        % Code for 2 parties. Comment out for 3 parties
+%         [isrep fMin a b]=localunitarilyequiv2(allM(:,:,i),knownM(:,:,k));
+%         if ~isrep [isrep fMin a b]=localunitarilyequiv2(allM(:,:,i),knownM(:,:,k));end
+%         if ~isrep
+%             % Search for equivalence of the swapped basis
+%             swapped = swap*allM(:,:,i);
+%             [isrep fMin a b]=localunitarilyequiv2(swapped,knownM(:,:,k));
+%         end
+%         if ~isrep [isrep fMin a b]=localunitarilyequiv2(swapped,knownM(:,:,k));end
+%         % Code for 3 parties. Comment out for 2 parties
+%         [isrep fMin a b]=localunitarilyequiv3(allM(:,:,i),knownM(:,:,k));
+%         if ~isrep
+%             swapped = swap213*allM(:,:,i);
+%             [isrep fMin a b]=localunitarilyequiv3(swapped,knownM(:,:,k));
+%         end
+%         if ~isrep
+%             swapped = swap321*allM(:,:,i);
+%             [isrep fMin a b]=localunitarilyequiv3(swapped,knownM(:,:,k));
+%         end
+%         if ~isrep
+%             swapped = swap132*allM(:,:,i);
+%             [isrep fMin a b]=localunitarilyequiv3(swapped,knownM(:,:,k));
+%         end
+%         if ~isrep
+%             swapped = swap231*allM(:,:,i);
+%             [isrep fMin a b]=localunitarilyequiv3(swapped,knownM(:,:,k));
+%         end
+%         if ~isrep
+%             swapped = swap312*allM(:,:,i);
+%             [isrep fMin a b]=localunitarilyequiv3(swapped,knownM(:,:,k));
+%         end
         if isrep
             unique=0;
             break
@@ -24,6 +66,7 @@ for i=1:size(allM,3)
     if unique
         nbSolutions=nbSolutions+1;
         knownM(:,:,nbSolutions) = allM(:,:,i);
-        save('threeEbitLocalizable.mat','knownM');
+        save('results/TwoPartiesThreeEbits.mat','knownM');   % Change for the different cases
     end
 end
+seventeenEbitLocalizable
